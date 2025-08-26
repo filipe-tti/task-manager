d9dcab37cb95e91241a14a50dc3de5e4b8736eb4
Filipe Silva <52262356+filipe-tti@users.noreply.github.com>
2025-08-26 20:55:22 -0300

    chore: add poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        brand: {
+          "dark-blue": "#35383E",
+          primary: "#00ADB5",
+          "dark-gray": "#818181",
+          "text-gray": "#9A9C9F",
+          "light-gray": "#EEEEEE",
+          white: "#FFFFFF",
+          background: "#F8F8F8",
+          border: "#F4F4F5",
+          process: "#FFAA04",
+          danger: "#EF4444",
+        },
+      },
+    },
   },
   plugins: [],
 }
-
