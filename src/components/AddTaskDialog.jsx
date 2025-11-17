aab58c99d6e35ae490806dfa6fe2dce2bc20e0e9
Filipe <filipe18tti@gmail.com>
2025-11-17 18:17:38 -0300

    chore: add tailwind-variants

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -117,7 +117,7 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
                   <Button
                     size="large"
                     className="w-full"
-                    variant="secondary"
+                    color="secondary"
                     onClick={handleClose}
                   >
                     Cancelar
